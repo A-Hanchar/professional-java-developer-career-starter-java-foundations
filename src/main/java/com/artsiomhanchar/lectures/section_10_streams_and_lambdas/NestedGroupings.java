04b4da450974301a1653e83176a849f91d06b4aa
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-11 19:06:07 +0400

    feat-passed-the lesson-176-nested-groupings

---
+++ src/main/java/com/artsiomhanchar/lectures/section_10_streams_and_lambdas/NestedGroupings.java
@@ -0,0 +1,59 @@
+package com.artsiomhanchar.lectures.section_10_streams_and_lambdas;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.NumberFormat;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class NestedGroupings {
+    final static File directory = new File("./\\src\\main\\java\\com\\artsiomhanchar\\lectures\\section_10_streams_and_lambdas\\Hr5m.csv");
+
+    record Person(String firstName, String lastName, long salary, String state, char gender) {}
+
+    public static void main(String[] args) {
+        try {
+            long startTime = System.currentTimeMillis();
+
+            Stream<String> fileLines = Files
+                    .lines(Path.of(directory.getAbsolutePath()));
+
+            TreeMap<String, Map<Character, String>> result = fileLines
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .map(array -> new Person(
+                            array[2],
+                            array[4],
+                            Long.parseLong(array[25]),
+                            array[32],
+                            array[5]
+                                    .strip()
+                                    .charAt(0)
+                    ))
+                    .collect(
+                            Collectors.groupingBy(
+                                    Person::state,
+                                    TreeMap::new,
+                                    Collectors.groupingBy(
+                                            Person::gender,
+                                            Collectors.collectingAndThen(
+                                                    Collectors.summingLong(Person::salary),
+                                                    NumberFormat.getCurrencyInstance()::format
+                                            )
+                                    )
+                            )
+                    );
+
+            long endTime = System.currentTimeMillis();
+
+            System.out.println(result);
+            System.out.println(endTime - startTime);
+        } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+    }
+}
