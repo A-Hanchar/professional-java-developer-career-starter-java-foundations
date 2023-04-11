9e2b679fe7172ec32f8eb3121abeb6978eac6b78
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-11 17:04:06 +0400

    feat-passed-the lesson-174-grouping-records

--- src/main/java/com/artsiomhanchar/lectures/section_10_streams_and_lambdas/GroupingRecords.java
+++ src/main/java/com/artsiomhanchar/lectures/section_10_streams_and_lambdas/GroupingRecords.java
@@ -4,13 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class DomainModelWithStreamsAPI {
+public class GroupingRecords {
     final static File directory = new File("./\\src\\main\\java\\com\\artsiomhanchar\\lectures\\section_10_streams_and_lambdas\\Hr5m.csv");
 
-    record Person(String firstName, String lastName, long salary) {}
+    record Person(String firstName, String lastName, long salary, String state) {}
 
     public static void main(String[] args) {
         try {
@@ -18,16 +21,18 @@ public class DomainModelWithStreamsAPI {
             Stream<String> fileLines = Files
                 .lines(Path.of(directory.getAbsolutePath()));
 
-            Long totalSalary = fileLines
+            Map<String, List<Person>> peoplesGroupedByState = fileLines
                     .parallel()
                     .skip(1)
+                    .limit(10)
                     .map(line -> line.split(","))
-                    .map(array -> new Person(array[2], array[4], Long.parseLong(array[25])))
-                    .collect(Collectors.summingLong(Person::salary));
+                    .map(array -> new Person(array[2], array[4], Long.parseLong(array[25]), array[32]))
+//                    .collect(Collectors.groupingBy(Person::state));
+                    .collect(Collectors.groupingBy(Person::state, TreeMap::new, Collectors.toList()));
 
             long endTime = System.currentTimeMillis();
 
-            System.out.printf("$%,d.00%n", totalSalary);
+            System.out.println(peoplesGroupedByState);
             System.out.println(endTime - startTime);
         } catch (IOException e) {
             throw new RuntimeException(e);
