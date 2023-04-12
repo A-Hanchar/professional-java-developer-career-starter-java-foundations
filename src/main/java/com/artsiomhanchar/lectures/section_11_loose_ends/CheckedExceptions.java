07c522b803d503a9ac5ee10a3af1e945dde2c12d
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-12 13:59:55 +0400

    feat-passed-the-lesson-183-checked-exceptions

---
+++ src/main/java/com/artsiomhanchar/lectures/section_11_loose_ends/CheckedExceptions.java
@@ -0,0 +1,19 @@
+package com.artsiomhanchar.lectures.section_11_loose_ends;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CheckedExceptions {
+    public static void main(String[] args) {
+        try {
+            Files.lines(Path.of("blahblahblah"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("We were unable to open the file");
+//            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Make sure this runs no matter what...");
+        }
+    }
+}
